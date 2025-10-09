d1979d3301768ce0138d2471b74777b58355eb88
Nephrit44 <nephrit44@yandex.ru>
2025-10-09 09:57:34 +0300

    Обновлены ссылки для звонков и групп

--- js/db.js
+++ js/db.js
@@ -3,209 +3,209 @@ const arrColletion = [
   {
     dataGroup: 101,
     groupName: "П-101СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=Y3YK9q6lMAzsRU4kaxqDQ3GHnFcflDloryg=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3PAMTw", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/bfOKGc1tiBUhlRg2Mz94ulYKkhlmvYJGcxCY3sLkwc8", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=hGBDrVDlP8k7v18aBI_F6mNU85jpFzFksLwnA5vwL14", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=Y3YK9q6lMAzsRU4kaxqDQ3GHnFcflDloryg=", 
+    maxGroupLink: "https://max.ru/join/bfOKGc1tiBUhlRg2Mz94ulYKkhlmvYJGcxCY3sLkwc8", 
     maxGroupCall: "https://max.ru/joincall/kui2Eukllaq6biqiLru_K_L_u_IgFaCTPWUZpcS4c6M",
   },
   {
     dataGroup: 102,
     groupName: "П-102СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=AEhw6sPtX7xBFcmM/_TDOcXJ4_kT_YKG58c=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3PAMiT", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/1m-Xo556OPkZe9kXNK4w2taNsitQUM9J3SteUfKplgc", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=T3-Qyq9azOacsn1v_yjeFmcLqYyYkEqcl6awKqGP4d0", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=AEhw6sPtX7xBFcmM/_TDOcXJ4_kT_YKG58c=", 
+    maxGroupLink: "https://max.ru/join/1m-Xo556OPkZe9kXNK4w2taNsitQUM9J3SteUfKplgc", 
     maxGroupCall: "https://max.ru/joincall/timhogd6MdjYO2GLkVeyt6jZXyj_GIRafo7wtn_ldiw",
   },
   {
     dataGroup: 103,
     groupName: "П-103СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=a6n8zH226Aj2iZFLK8m9bJawsk7V52djoeo=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3NyTGk", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/HthGPM3kkl0IJqcGMjangkPB7cbRA6ZylrHbjuSjyrQ", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=vUHCKB9Te6kZwzhPPP2pX-1Yayz9CqRSWLc6JVXscYs", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=a6n8zH226Aj2iZFLK8m9bJawsk7V52djoeo=", 
+    maxGroupLink: "https://max.ru/join/HthGPM3kkl0IJqcGMjangkPB7cbRA6ZylrHbjuSjyrQ", 
     maxGroupCall: "https://max.ru/joincall/t5vA7kHRO26nM2V8jH3jTS82DQks2lkM8V__Z8rBBfk",
   },
   {
     dataGroup: 104,
     groupName: "П-104СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=hq5UdxsB4F8oVnwuto/3H_HYNEBjcOqqf_A=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3NyTYz", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/b5faE5hs4X89sUerEnjqXYo_filek3TQ-5SH6LswTeE", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=tJLnWEEli4Hb2npzuFS5WSWir3mGpGQTSDthDkDK3_c", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=hq5UdxsB4F8oVnwuto/3H_HYNEBjcOqqf_A=", 
+    maxGroupLink: "https://max.ru/join/b5faE5hs4X89sUerEnjqXYo_filek3TQ-5SH6LswTeE", 
     maxGroupCall: "https://max.ru/joincall/R5Khjgaekm0Jh-CxdUz1yFv3NzBFvxG8PN1P7_DtGaA",
   },
   {
     dataGroup: 105,
     groupName: "П-105СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=pzE_/zO19h194_tsp/aCVLpkMEhzKdpjlr4=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3NyTfN", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/Uem7geFD4IPbMT6Rp_oZ-VNpxqPfdwJ_h5bayX3Zj7E", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=AqZn8jLpVl_JknCUhr9-UGCRN7qrO6kGjicvJ95Zfo8", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=pzE_/zO19h194_tsp/aCVLpkMEhzKdpjlr4=", 
+    maxGroupLink: "https://max.ru/join/Uem7geFD4IPbMT6Rp_oZ-VNpxqPfdwJ_h5bayX3Zj7E", 
     maxGroupCall: "https://max.ru/joincall/FQb3isnGLsPITmVMsaSJm98orMlKTCTJhLfsYYiLltI",
   },
   {
     dataGroup: 106,
     groupName: "П-106СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=mNp9fTKFG8g7LufJe_ujQJxpfuB06nC41Ko=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3NyTo4", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/Mowz9GeXaD8EbWqGErm-btVgeX6YPlBP26qtXR5fxv4", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=UScs3nNAtnPu-VNtfyHoeJIdgrNrxOIVG_EyraoPcNQ", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=mNp9fTKFG8g7LufJe_ujQJxpfuB06nC41Ko=", 
+    maxGroupLink: "https://max.ru/join/Mowz9GeXaD8EbWqGErm-btVgeX6YPlBP26qtXR5fxv4", 
     maxGroupCall: "https://max.ru/joincall/g7z20_9SZR9R_GdFvuUu6QGTdntkNoO4cDAYXyx6U2Y",
   },
   {
     dataGroup: 107,
     groupName: "П-107СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=1_W_xttNBML2ty3GeF3szSCCM2mFvms5faI=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3NyU3Y", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/k4vkoOcgYIgZPsQ13LlTETl-3QTL7XCB8j6c4LeS1Vo", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=7t8EV79X6zjbQvya5NJtFX9UM5cr8ZEt7HozRfqOylo", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=1_W_xttNBML2ty3GeF3szSCCM2mFvms5faI=", 
+    maxGroupLink: "https://max.ru/join/k4vkoOcgYIgZPsQ13LlTETl-3QTL7XCB8j6c4LeS1Vo", 
     maxGroupCall: "https://max.ru/joincall/QzBHOTViS-FOcccecuqvLsZ6VlZz42NgX_R4u9U5yHw",
   },
   {
     dataGroup: 108,
     groupName: "П-108СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=UlNfXrsEWPsJDM5jQjnvhyCpHi60CpfbUKI=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3NyVZW", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/oACIwHAy3dUQYmyNt5Vexf7CySkfZqVCRt5nH96zMLI", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=vxqOhhgkgqILbEowcy42HRnz9kMxdLscuh5xBR6tyQY", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=UlNfXrsEWPsJDM5jQjnvhyCpHi60CpfbUKI=", 
+    maxGroupLink: "https://max.ru/join/oACIwHAy3dUQYmyNt5Vexf7CySkfZqVCRt5nH96zMLI", 
     maxGroupCall: "https://max.ru/joincall/t9CrKQemRnBOfW0xdG0i4oNXx10t0rZVJZu87oDaoxk",
   },
   {
     dataGroup: 109,
     groupName: "П-109СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=uAwRspqvCaOjDfFoKLMLH30HbiM2MJ9C9VU=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3NyXG4", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/GfdcDG6SH4EAFsIsn7dCOln0QDQv7pUAWhR0eNUGoy8", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=XGFqBL9_vnLLkoA7eFY0_Id7173qdbx071gboy7JnrA", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=uAwRspqvCaOjDfFoKLMLH30HbiM2MJ9C9VU=", 
+    maxGroupLink: "https://max.ru/join/GfdcDG6SH4EAFsIsn7dCOln0QDQv7pUAWhR0eNUGoy8", 
     maxGroupCall: "https://max.ru/joincall/-0NYMlG2G6tQfOCJm9Ra4u8zRBaVQLgZK2KvRkheKfc",
   },
   {
     dataGroup: 110,
     groupName: "П-110СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=Ju9bjUAMZEp_0ThL2oKLYJKmJD4GN/RZpqM=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3NyXPi", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/jqhHXGalsCaX8RvcmX0CP7KkVxmlG2e3omuIZ2qUzGE", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=sCgZlSdxsSk_qzi70G89HyLVld6MS9S91vFkH3GkwHI", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=Ju9bjUAMZEp_0ThL2oKLYJKmJD4GN/RZpqM=", 
+    maxGroupLink: "https://max.ru/join/jqhHXGalsCaX8RvcmX0CP7KkVxmlG2e3omuIZ2qUzGE", 
     maxGroupCall: "https://max.ru/joincall/rGR3qL4vh5wM5Q4lpwJqyNT-VN0tRaupo1xp-1KokZs",
   },
   {
     dataGroup: 111,
     groupName: "П-111СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=2C1V9dsy62RO9xMwaXP6tqRfMGsd6eaY/b4=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3NyXns", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/yYn6Cx65zCzlZHFobR5qP695exZXr97dAiKZh4TV7Fg", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=4qg2cgVEzTWMl6lSIRlKoMGWfhmDNHdFWQHpseogl4M", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=2C1V9dsy62RO9xMwaXP6tqRfMGsd6eaY/b4=", 
+    maxGroupLink: "https://max.ru/join/yYn6Cx65zCzlZHFobR5qP695exZXr97dAiKZh4TV7Fg", 
     maxGroupCall: "https://max.ru/joincall/AjnGzzDrz0PT16dZMGYJNc_0SiTpbJRDDyP2LYAddEk",
   },
   {
     dataGroup: 112,
     groupName: "П-112СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=aggGIo2MVJrkpGlUEDxzyYFqpvZqGAjOpLo=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://sferum.ru?call_link=BklgSU_k9ge-YxLYXtDcAnYCBNJihitFfNJkvtRBbB0", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/uqA9q8i1hNryh_O3YN5zSGwn0LPPcTaygvctxBCUutQ", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=aWENMBmJ47M8OhNLl8ApyIc3KjkaRoi5Olju41mNqa0", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=aggGIo2MVJrkpGlUEDxzyYFqpvZqGAjOpLo=", 
+    maxGroupLink: "https://max.ru/join/uqA9q8i1hNryh_O3YN5zSGwn0LPPcTaygvctxBCUutQ", 
     maxGroupCall: "https://max.ru/joincall/AjnGzzDrz0PT16dZMGYJNc_0SiTpbJRDDyP2LYAddEk",
   },
   {
     dataGroup: 113,
     groupName: "П-113СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=Q4KXWo66PSUsRmgqNiHYNybJR2TluovgqQE=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3NyXyv", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/uy79GDVvPVpEz69pAN6ou6SYVGqNbD5JQ2jsusQcoO4", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=Rc7f2Mbai2f3rtuX_aVzpX8h37ETPhtIaxPXexyDhPI", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=Q4KXWo66PSUsRmgqNiHYNybJR2TluovgqQE=", 
+    maxGroupLink: "https://max.ru/join/uy79GDVvPVpEz69pAN6ou6SYVGqNbD5JQ2jsusQcoO4", 
     maxGroupCall: "https://max.ru/joincall/Y32c0D3Cqu0NByAax0UUCFVTY8og_nps9vz0rwcOzHw",
   },
   {
     dataGroup: 114,
     groupName: "П-114СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=wwRgfyibK3JQIA4gytuqWEIDafbQrHdILpg=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3NyYLX", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/iVZJfvCtR36fjouseZVld_rhT-OLyOZwskivmFe_Ius", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=yXgi3sKOLebDm8aachjPA5ch9jB_v8ryxCqKxun0SIk", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=wwRgfyibK3JQIA4gytuqWEIDafbQrHdILpg=", 
+    maxGroupLink: "https://max.ru/join/iVZJfvCtR36fjouseZVld_rhT-OLyOZwskivmFe_Ius", 
     maxGroupCall: "https://max.ru/joincall/5tgFl2Q4ZD23zGX01_bkO_pcshmSWR3tsCVXwvxcufQ",
   },
   {
     dataGroup: 115,
     groupName: "П-115СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=pHx/bzfB4tJimTjwtqQ00Awlj/KsG2gf9rE=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3NyYSf", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/s_jnKRVZ7ahlLJQSy636cNKuBt736VRwpK3xBXcwN2Y", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=DTSel-J7jKrlz3eqFJ1piHem-5GbHHKvQZNCzC6enkY", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=pHx/bzfB4tJimTjwtqQ00Awlj/KsG2gf9rE=", 
+    maxGroupLink: "https://max.ru/join/s_jnKRVZ7ahlLJQSy636cNKuBt736VRwpK3xBXcwN2Y", 
     maxGroupCall: "https://max.ru/joincall/F6V5pAP1-SeLbRPJv9jEoEVaeSONN0dQGrE01wM5xWY",
   },
   {
     dataGroup: 116,
     groupName: "П-116СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=6YSTNxV_nR0WwY83AhOTO5r2eqqzVMXAWPM=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3NyYzN", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/vk1QQYmwSzTWtPnLu7wprseUkCm0AqgdNK6ehr684Uo", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=YgIZDJYB1ctJEjhzCThPxxaRr7_0kLbAXlQUMlbwfbc", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=6YSTNxV_nR0WwY83AhOTO5r2eqqzVMXAWPM=", 
+    maxGroupLink: "https://max.ru/join/vk1QQYmwSzTWtPnLu7wprseUkCm0AqgdNK6ehr684Uo", 
     maxGroupCall: "https://max.ru/joincall/ffBDygGSLKXvbm7mNXWi8YOjdulHCQLhJuYwNAjfE2M",
   },
   {
     dataGroup: 117,
     groupName: "П-117СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=y7fI8Z73u3mso3NJzZIQAwlitLPRRiEunQg=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3NyqnY", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/TgFyS88_Z0CH5rvNAgDHQ3Fhs3rDYqcCHczySvkn1hc", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=nx-niHxsGItVabSSXF9y4vb8_E6yAj-S4sS77nQGwus", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=y7fI8Z73u3mso3NJzZIQAwlitLPRRiEunQg=", 
+    maxGroupLink: "https://max.ru/join/TgFyS88_Z0CH5rvNAgDHQ3Fhs3rDYqcCHczySvkn1hc", 
     maxGroupCall: "https://max.ru/joincall/bygOWf0i5TRBQ519l6sqKySpFL475mn_4kyBJeKszJM",
   },
   {
     dataGroup: 118,
     groupName: "П-118СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=uz5hFmurc7fhtgndlX2PoL1Vm2r8NFTij80=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3Nyr62", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/7P9hFsWpV3vw4klaQPr2TxjaRpYUX3wYvfw5kg3FG8w", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=0kW78CU7sjoTjN9OkhwWkkHm9tGgu-OyJKxsZItskR8", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=uz5hFmurc7fhtgndlX2PoL1Vm2r8NFTij80=", 
+    maxGroupLink: "https://max.ru/join/7P9hFsWpV3vw4klaQPr2TxjaRpYUX3wYvfw5kg3FG8w", 
     maxGroupCall: "https://max.ru/joincall/lG0vEQpmznJVQGYx9rG9YhABAP99NlONLKeub2ycS8k",
   },
   {
     dataGroup: 119,
     groupName: "П-119СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=WHs42ter6nwtXHzUPb/qEeRii9ewmeXzJqo=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3NyrAi", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/tl0Trcrna5CVeRbo614A6oS2oeW1Yqt_uFctqsSHZf8", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=Y9KfLikUnHluYEFy_qyynqdguU2vq2tEGG3uqjOeuho", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=WHs42ter6nwtXHzUPb/qEeRii9ewmeXzJqo=", 
+    maxGroupLink: "https://max.ru/join/tl0Trcrna5CVeRbo614A6oS2oeW1Yqt_uFctqsSHZf8", 
     maxGroupCall: "https://max.ru/joincall/iTndvYTgZtcWzqtisEPuupvNE6Mjiy7Xm1q6cFslyuI",
   },
   {
     dataGroup: 120,
     groupName: "П-120СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=DtBWH_gw4FMdARxlxwB1U4SfueT9PhGUrL4=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3NyrDq", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/-tSwl6VMr8MthVLdZyt_COJptxVOEvtXxouTrhTo1NI", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=qwD-FKMupekt4Cm8rJKqkRCUdmdFbY3Uz5L2dGHUoJU", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=DtBWH_gw4FMdARxlxwB1U4SfueT9PhGUrL4=", 
+    maxGroupLink: "https://max.ru/join/-tSwl6VMr8MthVLdZyt_COJptxVOEvtXxouTrhTo1NI", 
     maxGroupCall: "https://max.ru/joincall/dNs8LM-r6mfZkU4xPOT10xs0F16sl5B8JnoK9ELBnrw",
   },
   {
     dataGroup: 121,
     groupName: "П-121СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=Bk0nFZbjn2urrxgsjKLT3jdtrAkqbo5xMak=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3NyrH3", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/UL8b1bTIK1CIeH2kGk5lZsqLBgSbmURKU5lVyTu3bbk", //Ссылка на группу в МАКС
+    sferumGroupCall: "", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=Bk0nFZbjn2urrxgsjKLT3jdtrAkqbo5xMak=", 
+    maxGroupLink: "https://max.ru/join/UL8b1bTIK1CIeH2kGk5lZsqLBgSbmURKU5lVyTu3bbk", 
     maxGroupCall: "https://max.ru/joincall/xU09UdoMuTUMsQhv6Cl_JOSS_DRa2y6siTmEYS29k_c",
   },
   {
     dataGroup: 122,
     groupName: "П-122СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=SJR2qLuou7LLvr1cG1FihvvwGKoyvxpIrdA=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3PANun", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/PZsv2EvM6exlBEWA3mUZEGi7gDaVP2HcPVCcPg49Uk0", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=rcIVQ3ZPbYm2lZrrh5ziDnz_hHZg8b2OQgZpk7KrVV8", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=SJR2qLuou7LLvr1cG1FihvvwGKoyvxpIrdA=", 
+    maxGroupLink: "https://max.ru/join/PZsv2EvM6exlBEWA3mUZEGi7gDaVP2HcPVCcPg49Uk0", 
     maxGroupCall: "https://max.ru/joincall/4nT7CGsi1epkrY2vQT-F7ZAv0XCo5oXe9X5L5LU5lBg",
   },
   {
     dataGroup: 123,
     groupName: "П-123СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=dvfvvJS5o1VWcQ3etMhMOkjrs4_4E/6pFgY=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://sferum.ru?call_link=W-HUPE_-0JO6DJZAv9KR9096cKuSQf6Yj_9kUbFshR8", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/4y-pvsbktnSeh83wcvCgWgAJwlmzvVqQqlLFYYqinAw", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=RCfAcwPwbtfWDzujZYvQ2YT5iOfZPFb9MnI-ejgl6mQ", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=dvfvvJS5o1VWcQ3etMhMOkjrs4_4E/6pFgY=", 
+    maxGroupLink: "https://max.ru/join/4y-pvsbktnSeh83wcvCgWgAJwlmzvVqQqlLFYYqinAw", 
     maxGroupCall: "https://max.ru/joincall/psHfKtz3ccHmxKcQWJafivjAgu9kauo6m1TwY-UE2E4",
   },
   {
     dataGroup: 124,
     groupName: "П-124СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=WR_sGHnBqpQjdQMmP1Cpwvi_zbPoOXCqwHM=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://clck.ru/3NyrSm", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/RIgTs4GyyjeKn2hn6S4gWKIPo4TRI_JpE6mk-QDC9xM", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=T9Nxmc8PatbemK5YOKpDzWfEKHwXksn3JvtXZibK9Gw", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=WR_sGHnBqpQjdQMmP1Cpwvi_zbPoOXCqwHM=", 
+    maxGroupLink: "https://max.ru/join/RIgTs4GyyjeKn2hn6S4gWKIPo4TRI_JpE6mk-QDC9xM", 
     maxGroupCall: "https://max.ru/joincall/BvNK73atNzw_eG_ReJhfx4Qc0diaCdXENhRJgcwr6ow",
   },
   {
     dataGroup: 125,
     groupName: "П-125СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=T6Zgn/oXeA9zwpKNHtuYfXu3PNiJwuArgHE=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://sferum.ru?call_link=5PWIgEl9T_uP3UVIViMVII6reG2Df2yomUXYCiSUw6Y", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/8G6KG3esYCSgk8aVWZv1y6QbZi0xgarfV39taEcAH_Q", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=46NswjVEeGtoNDerzRIJ_a7vmKJK-OjhXpjKaMA-66k", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=T6Zgn/oXeA9zwpKNHtuYfXu3PNiJwuArgHE=", 
+    maxGroupLink: "https://max.ru/join/8G6KG3esYCSgk8aVWZv1y6QbZi0xgarfV39taEcAH_Q", 
     maxGroupCall: "https://max.ru/joincall/Wi_8IpXVa6tm2vvDpP69GDZwD4Sn28b13bicOHtMIkU",
   },
   {
     dataGroup: 126,
     groupName: "П-126СД",
-    sferumGroupCall: "https://sferum.ru/?p=messages&join=qWNc_K/Oqi9hS1vFVmEWXu1i6PxDym6WAs8=", //Ссылка на группу в сферум
-    sferumGroupLink: "https://sferum.ru?call_link=yELksx8_5jrRD-tSHPAuh5U9Qx9i-TQawTjkEwqArr0", //ссылка на звонок в сферум
-    maxGroupLink: "https://max.ru/join/9tnFOTiC_KIuFzYtT68hfDNfxyD59_eDovByn1it1Qw", //Ссылка на группу в МАКС
+    sferumGroupCall: "https://sferum.ru?call_link=P1acNt3vQxsLdi2V9pw8BB7mDMeWjUUZ7GPAqeTG9K8", 
+    sferumGroupLink: "https://sferum.ru/?p=messages&join=qWNc_K/Oqi9hS1vFVmEWXu1i6PxDym6WAs8=", 
+    maxGroupLink: "https://max.ru/join/9tnFOTiC_KIuFzYtT68hfDNfxyD59_eDovByn1it1Qw", 
     maxGroupCall: "https://max.ru/joincall/X38-oikgYFiqFIDZwCf4vmuTF2z6jzFo3tAmqnotyQ8",
   }
 ]; 
